b7ac0564969e1dabd2815191ad895670e8552725
noumo <noumohope@gmail.com>
2015-04-19 00:42:07 +0300

    code clean

--- behaviors/SeoBehavior.php
+++ behaviors/SeoBehavior.php
@@ -52,41 +52,16 @@ class SeoBehavior extends \yii\base\Behavior
         return $this->owner->hasOne(SeoText::className(), ['item_id' => $this->owner->primaryKey()[0]])->where(['model' => get_class($this->owner)]);
     }
 
-    public function getSeo_h1()
-    {
-        return $this->seoText->h1;
-    }
-
-    public function getSeo_title()
-    {
-        return $this->seoText->title;
-    }
-
-    public function getSeo_keywords()
-    {
-        return $this->seoText->keywords;
-    }
-
-    public function getSeo_description()
-    {
-        return $this->seoText->description;
-    }
-
     public function getSeoText()
     {
         if(!$this->_model)
         {
-            if($this->owner && $this->owner->primaryKey) {
-                $itemModel = get_class($this->owner);
-                $this->_model = SeoText::findOne(['model' => $itemModel, 'item_id' => $this->owner->primaryKey]);
-                if(!$this->_model){
-                    $this->_model = new SeoText([
-                        'model' => $itemModel,
-                        'item_id' => $this->owner->primaryKey
-                    ]);
-                }
-            } else {
-                $this->_model = new SeoText();
+            $this->_model = $this->owner->seo;
+            if(!$this->_model){
+                $this->_model = new SeoText([
+                    'model' => get_class($this->owner),
+                    'item_id' => $this->owner->primaryKey
+                ]);
             }
         }
 
