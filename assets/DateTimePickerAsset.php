b573947264c027cc5afbe01e5d9997ccbc24b315
noumo <noumohope@gmail.com>
2015-05-17 02:06:22 +0300

    new widget

---
+++ assets/DateTimePickerAsset.php
@@ -0,0 +1,28 @@
+<?php
+namespace yii\easyii\assets;
+
+class DateTimePickerAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower';
+    public $depends = [
+        'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+
+    public function init()
+    {
+        if (YII_DEBUG) {
+            $this->js[] = 'moment\min\moment-with-locales.js';
+            $this->js[] = 'bootstrap-datepicker\dist\js\bootstrap-datepicker.js';
+            $this->js[] = 'eonasdan-bootstrap-datetimepicker\src\js\bootstrap-datetimepicker.js';
+            $this->css[] = 'eonasdan-bootstrap-datetimepicker\build\css\bootstrap-datetimepicker.css';
+        } else {
+            $this->js[] = 'moment\min\moment-with-locales.min.js';
+            $this->js[] = 'bootstrap-datepicker\dist\js\bootstrap-datepicker.min.js';
+            $this->js[] = 'eonasdan-bootstrap-datetimepicker\build\js\bootstrap-datetimepicker.min.jsredactor.min.js';
+            $this->css[] = 'eonasdan-bootstrap-datetimepicker\build\css\bootstrap-datetimepicker.min.css';
+        }
+    }
+
+}
