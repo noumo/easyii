6472c35ad7bb43b3aecfe121ced220dd088e97cc
evgenmil <joni_mil@mail.ru>
2015-05-26 13:25:33 +0600

    Update _form.php
    Fix url

--- modules/news/views/a/_form.php
+++ modules/news/views/a/_form.php
@@ -18,7 +18,7 @@ $module = $this->context->module->id;
 <?php if($this->context->module->settings['enableThumb']) : ?>
     <?php if($model->image) : ?>
         <img src="<?= Image::thumb($model->image, 240) ?>">
-        <a href="'<?= Url::to(['/admin/'.$module.'/a/clear-image', 'id' => $model->news_id]) ?>" class="text-danger confirm-delete" title="<?= Yii::t('easyii', 'Clear image')?>"><?= Yii::t('easyii', 'Clear image')?></a>
+        <a href="<?= Url::to(['/admin/'.$module.'/a/clear-image', 'id' => $model->news_id]) ?>" class="text-danger confirm-delete" title="<?= Yii::t('easyii', 'Clear image')?>"><?= Yii::t('easyii', 'Clear image')?></a>
     <?php endif; ?>
     <?= $form->field($model, 'image')->fileInput() ?>
 <?php endif; ?>
