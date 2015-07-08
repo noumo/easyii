c1034eee2a8be07f059e7652a248112484dffc78
noumo <noumohope@gmail.com>
2015-07-08 23:41:43 +0300

    install url fix

--- views/install/_form.php
+++ views/install/_form.php
@@ -3,7 +3,7 @@ use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
-<?php $form = ActiveForm::begin(['action' => Url::to('/admin/install')]); ?>
+<?php $form = ActiveForm::begin(['action' => Url::to(['/admin/install'])]); ?>
 <?= $form->field($model, 'root_password', ['inputOptions' => ['title' => Yii::t('easyii/install','Password to login as root')]]) ?>
 <?= $form->field($model, 'admin_email', ['inputOptions' => ['title' => Yii::t('easyii/install','Used as "ReplyTo" in mail messages')]]) ?>
 <?= $form->field($model, 'robot_email', ['inputOptions' => ['title' => Yii::t('easyii/install','Used as "From" in mail messages')]]) ?>
