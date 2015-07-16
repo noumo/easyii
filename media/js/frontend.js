4f4ac471538c8e5459c4acc0271bbcc1fa1ed9c5
phplego <phplego@gmail.com>
2015-07-17 02:39:16 +0300

    removed target=_blank

--- media/js/frontend.js
+++ media/js/frontend.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.easyiicms-edit').each(function(i, element){
         var $this = $(element);
-        $this.append('<a href="'+$this.data('edit')+'" class="easyiicms-goedit" style="width: '+$this.width()+'px; height: '+$this.height()+'px;" target="_blank"></a>');
+        $this.append('<a href="'+$this.data('edit')+'" class="easyiicms-goedit" style="width: '+$this.width()+'px; height: '+$this.height()+'px;"></a>');
     });
     $('#easyii-navbar input').switcher({copy: {en: {yes: '', no: ''}}}).on('change', function(){
         var checkbox = $(this);
