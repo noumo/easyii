79c88292acd713a83812691eaa0c8dc10ceb9110
boehsermoe <boehsermoe@me.com>
2015-12-12 18:03:17 +0100

    Content Behavior with view title

--- modules/content/behaviors/ContentController.php
+++ modules/content/behaviors/ContentController.php
@@ -2,6 +2,8 @@
 
 namespace yii\easyii\modules\content\behaviors;
 
+use yii;
+use yii\base\ActionEvent;
 use yii\easyii\modules\content\api\Content;
 use yii\easyii\modules\content\api\ItemObject;
 use yii\easyii\modules\content\models\Item;
@@ -20,6 +22,48 @@ class ContentController extends Behavior
 {
 
     private $_content;
+
+    /**
+     * Declares event handlers for the [[owner]]'s events.
+     *
+     * Child classes may override this method to declare what PHP callbacks should
+     * be attached to the events of the [[owner]] component.
+     *
+     * The callbacks will be attached to the [[owner]]'s events when the behavior is
+     * attached to the owner; and they will be detached from the events when
+     * the behavior is detached from the component.
+     *
+     * The callbacks can be any of the following:
+     *
+     * - method in this behavior: `'handleClick'`, equivalent to `[$this, 'handleClick']`
+     * - object method: `[$object, 'handleClick']`
+     * - static method: `['Page', 'handleClick']`
+     * - anonymous function: `function ($event) { ... }`
+     *
+     * The following is an example:
+     *
+     * ~~~
+     * [
+     *     Model::EVENT_BEFORE_VALIDATE => 'myBeforeValidate',
+     *     Model::EVENT_AFTER_VALIDATE => 'myAfterValidate',
+     * ]
+     * ~~~
+     *
+     * @return array events (array keys) and the corresponding event handler methods (array values).
+     */
+    public function events()
+    {
+        return [
+            Controller::EVENT_BEFORE_ACTION => 'handleBeforeAction'
+        ];
+    }
+
+    public function handleBeforeAction(ActionEvent $event)
+    {
+        if ($this->content->model) {
+            $this->owner->view->title = Yii::$app->name . " - " . $this->content->seo('title', $this->content->model->title);
+        }
+    }
 
     /**
      * @return ItemObject
