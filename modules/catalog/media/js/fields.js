5fa1fdc3ac36f6bcd31897a75bdf092a49cf2ff3
NidgetGod <nidgetgod@hotmail.com>
2016-01-01 01:26:47 +0800

    Fixed #103 single quote bug

--- modules/catalog/media/js/fields.js
+++ modules/catalog/media/js/fields.js
@@ -53,7 +53,8 @@ $(function(){
                 options : $this.find('.field-options').val()
             };
             if(data.name != '') {
-                form += '<input type="hidden" name="Field[' + i + ']" value=\'' + JSON.stringify(data) + '\'>';
+                form += $('<input type="hidden" name="Field[' + i + ']">').val(JSON.stringify(data)).clone().wrap('<div/>').parent().html();
+                //form += '<input type="hidden" name="Field[' + i + ']" value=\'' + JSON.stringify(data) + '\'>';
             }
         });
         $('<form method="post">' + form + '</form>').appendTo('body').submit();
