9a3d02608028c2c23e1ee5860e6a530e0397d72a
Ivan Vasilkov <noumohope@gmail.com>
2016-04-23 01:00:05 +0300

    add children

--- modules/page/api/PageObject.php
+++ modules/page/api/PageObject.php
@@ -1,13 +1,13 @@
 <?php
 namespace yii\easyii\modules\page\api;
 
-use Yii;
 use yii\easyii\components\API;
 use yii\helpers\Url;
 
 class PageObject extends \yii\easyii\components\ApiObject
 {
     public $slug;
+    private $_children;
 
     public function getTitle(){
         return LIVE_EDIT ? API::liveEdit($this->model->title, $this->editLink) : $this->model->title;
@@ -15,6 +15,17 @@ class PageObject extends \yii\easyii\components\ApiObject
 
     public function getText(){
         return LIVE_EDIT ? API::liveEdit($this->model->text, $this->editLink, 'div') : $this->model->text;
+    }
+
+    public function getChildren()
+    {
+        if($this->_children === null) {
+            $this->_children = [];
+            foreach ($this->model->children as $child) {
+                $this->_children[] = Page::get($child);
+            }
+        }
+        return $this->_children;
     }
 
     public function getEditLink(){
