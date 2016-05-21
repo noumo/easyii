b995b13c37b70177c5c73589aaa1210527b79562
boehsermoe <boehsermoe@me.com>
2016-05-21 21:12:05 +0200

    JqueryUiAsset

--- assets/JqueryUiAsset.php
+++ assets/JqueryUiAsset.php
@@ -12,7 +12,7 @@ use yii\web\AssetBundle;
  */
 class JqueryUiAsset extends AssetBundle
 {
-	public $sourcePath = '@bower/jquery-ui/dist';
+	public $sourcePath = '@bower/jquery-ui';
 	public $js = [
 		'jquery-ui.js',
 	];
