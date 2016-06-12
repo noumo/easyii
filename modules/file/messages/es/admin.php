e900507bb9e4ea1debd6b60f96807a7a7b2dc218
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:38:14 -0500

    create admin.php español file

---
+++ modules/file/messages/es/admin.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'Files' => 'Archivos',
+    'Create file' => 'Crear archivo',
+    'Edit file' => 'Editar archivo',
+    'File created' => 'Archivo creado',
+    'File updated' => 'Archivo actualizado',
+    'File error. {0}' => 'Error de archivo. {0}',
+    'File deleted' => 'Archivo eliminado',
+
+    'Size' => 'Tamaño',
+    'Downloads' => 'Descargas',
+    'Download file' => 'Descargar archivo',
+];
