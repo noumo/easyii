19132e16ce1dbe4ff5276ec8c092c20cccc421b5
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:35:32 -0500

    create admin.php español feedback

---
+++ modules/feedback/messages/es/admin.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Feedback' => 'Comentarios',
+    'View feedback' => 'Ver comentarios',
+    'Feedback deleted' => 'Comentario borrado',
+    'Feedback updated' => 'Comentario actualizado',
+    'Answer' => 'Respuesta',
+    'Subject' => 'Asunto',
+    'Phone' => 'Teléfono',
+    'No answer' => 'No hay respuesta',
+    'Hello,' => 'Hola,',
+    'Answer on your feedback message' => 'Responder a su mensaje de comentario',
+    'Best regards.' => 'Saludos.',
+    'Answer successfully sent' => 'Respuesta enviada con éxito',
+    'Mark as answered' => 'Marca como respuesta',
+    'feedback answer' => 'respuesta de comentario',
+    'Please fill correct `Admin E-mail` in Settings' => 'Por favor llene "Admin correo electrónico correcta" en ajustes',
+    'Are you sure you want to resend the answer?' => '¿Está seguro que desea enviar la respuesta?',
+    'An error has occurred while sending mail' => 'Ha ocurrido un error al enviar el correo',
+];
