3d57cc2d22df064be109953b970466cd8952ce24
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:28:00 -0500

    create admin.php español carrousel

---
+++ modules/carousel/messages/es/admin.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'Carousel' => 'Carrusel',
+    'Create carousel' => 'Crear Carrusel',
+    'Edit carousel' => 'Editar Carrusel',
+    'Carousel created' => 'Carrusel creado',
+    'Carousel updated' => 'Carrusel actualizado',
+    'Carousel item deleted' => 'Elemento del carrusel eliminado',
+    'Link'=>'Enlance',
+];
