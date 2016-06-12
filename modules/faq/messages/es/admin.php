6b8c505a80a9256fbe6625d9dbcb43c4231bb8e1
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:33:32 -0500

    create admin.php español faq

---
+++ modules/faq/messages/es/admin.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'FAQ' => 'Preguntas más frecuentes',
+    'Question' => 'Pregunta',
+    'Answer' => 'Respuesta',
+    'Create entry' => 'Crear entrada',
+    'Edit entry' => 'Editar entrada',
+    'Entry created' => 'Entrada creada',
+    'Entry updated' => 'Entrada actualizada',
+    'Entry deleted' => 'Entrada eliminada'
+];
