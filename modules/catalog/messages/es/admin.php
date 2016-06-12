9926bfd8b7a4dcf6481aa73b48cb2deb602d78b8
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:29:58 -0500

    create admin.php español catalog

---
+++ modules/catalog/messages/es/admin.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'Catalog' => 'Catálogo',
+    'Category fields' => 'Campos de categoría',
+    'Manage fields' => 'Administrar campos',
+
+    'Add field' => 'Agregar campo',
+    'Save fields' => 'Guardar campos',
+    'Type options with `comma` as delimiter' => 'Tipo opciones con `coma` como delimitador',
+    'Fields' => 'Campos',
+
+    'Item created' => 'Elemento creado',
+    'Item updated' => 'Elemento actualizado',
+    'Item deleted' => 'Elemento eliminado',
+
+    'Title' => 'Título',
+    'Type' => 'Tipo',
+    'Options' => 'Opciones',
+    'Available' => 'Disponibles',
+    'Price' => 'Precio',
+    'Discount' => 'Descuento',
+    'Select' => 'Seleccione',
+];
