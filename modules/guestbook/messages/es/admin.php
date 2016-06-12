7743c15c634546d22cf4f53bbeda2e6d774aa544
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:43:00 -0500

    create admin.php español guestbook

---
+++ modules/guestbook/messages/es/admin.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'Guestbook' => 'Libro de visitas',
+    'Guestbook updated' => 'Libro de Visitas actualizado',
+    'View post' => 'Ver el post',
+    'Entry deleted' => 'entrada eliminada',
+    'Answer' => 'Responder',
+    'No answer' => 'Sin respuesta',
+    'Mark all as viewed' => 'Marcar todo como visto',
+    'Answer successfully saved' => 'Respuesta guardada correctamente',
+    'Mark as new' => 'Marcar como nuevo',
+    'Notify user about answer' => 'Notificar al usuario sobre la respuesta'
+];
