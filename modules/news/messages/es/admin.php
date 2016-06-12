05a0b6b7223bf4f56df480c32a9112569f39d0a2
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:46:49 -0500

    create admin.php español news

---
+++ modules/news/messages/es/admin.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'News' => 'Noticias',
+    'Create news' => 'Crear noticia',
+    'Edit news' => 'Modificar noticia',
+    'News created' => 'Noticia creada',
+    'News updated' => 'Noticia actualizada',
+    'News deleted' => 'Noticia eliminada',
+    'News image cleared' => 'Imagen de noticia eliminada',
+    'Short' => 'Corto',
+    'Clear image' => 'Quitar imagen',
+];
