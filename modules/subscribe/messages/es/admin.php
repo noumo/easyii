8b16a2401816add8470ada8c1b18192d9faf382c
andres2050 <andres.2050@hotmail.com>
2016-06-12 16:57:37 -0500

    create admin.php español subscribe

---
+++ modules/subscribe/messages/es/admin.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'Subscribe' => 'Suscribirse',
+    'Subscribers' => 'Suscriptores',
+    'Create subscribe' => 'Crear suscriptor',
+    'History' => 'Historia',
+    'View subscribe history' => 'Ver historia del suscriptor',
+    'Subscriber deleted' => 'Suscriptor eliminado',
+    'Subscribe successfully created and sent' => 'Suscriptor creado y enviado',
+    'Subject' => 'Tema',
+    'Body' => 'Cuerpo',
+    'Sent' => 'Expedido',
+    'Unsubscribe' => 'Dar de baja'
+];
