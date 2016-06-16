bf2fa3eda264282027ad56d76b42a64f585af154
deffyc <deffyc@users.noreply.github.com>
2016-06-17 00:13:28 +0800

    Create install.php

---
+++ messages/zh/install.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Installation' => '安装',
+    'Install' => '安装',
+    'Root password' => 'root密码',
+    'Admin E-mail' => '管理员电子邮件',
+    'Robot E-mail' => '发件地址',
+    'Auth time' => '认证时间',
+    'Frontend toolbar position' => '工具栏前台位置',
+    'Password to login as root' => '使用root登录的密码',
+    'Used as "ReplyTo" in mail messages' => '在邮件中作为“ReplyTo”的地址',
+    'Used as "From" in mail messages' => '在邮件中作为"From" 的地址',
+    'Required for using captcha in forms (guestbook, feedback)' => '在表单中要求使用验证码的（留言，反馈）',
+    'You easily can get keys on' => '你可以很容易的得到钥匙在',
+    'ReCaptcha website' => '重新验证网站',
+    'Installation completed' => '安装完成',
+    'Installation error' => '安装错误',
+    'Cannot connect to database. Please configure `{0}`.' => '无法连接到数据库。请配置 `{0}`.',
+    'EasyiiCMS is already installed. If you want to reinstall easyiiCMS, please drop all tables with prefix `easyii_` from your database manually.' => 'easyiicms已经安装。如果你想重新安装easyiicms，在您的数据库手动删除所有前缀为` easyii_ `的表。',
+
+];
