9d44337d7cb62b890fd324a16cb40af1d5d3cb58
Allan Bomfim <allan@interativasoftware.com.br>
2016-06-19 06:54:24 -0300

    Translation to pt-BR - install.pho

---
+++ messages/pt-BR/install.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'Installation' => 'Instalação',
+    'Install' => 'Instalar',
+    'Root password' => 'Senha de root',
+    'Admin E-mail' => 'E-mail do Admin',
+    'Robot E-mail' => 'E-mail Automático',
+    'Auth time' => 'Hora da autenticação',
+    'Frontend toolbar position' => 'Posição do toolbar',
+    'Password to login as root' => 'Senha do login como root',
+    'Used as "ReplyTo" in mail messages' => 'Usado como "ReplyTo" nas mensagens de email',
+    'Used as "From" in mail messages' => 'Usado como "From" nas mensagens de email',
+    'Required for using captcha in forms (guestbook, feedback)' => 'Obrigatório para uso do captcha nos formulários (guestbook, feedback)',
+    'You easily can get keys on' => 'Você pode facilmente pegar sua chave',
+    'ReCaptcha website' => 'ReCaptcha',
+    'Installation completed' => 'Instalação finalizada',
+    'Installation error' => 'Erro de instalaçao',
+    'Cannot connect to database. Please configure `{0}`.' => 'Não pode conectar ao banco de dados. Por favor configure `{0}`.',
+    'EasyiiCMS is already installed. If you want to reinstall easyiiCMS, please drop all tables with prefix `easyii_` from your database manually.' => 'EasyiiCMS já está instalado. Se você quiser reinstalar easyiiCMS, por favor exclua manualmente todas as tabelas com prefixo `easyii_` do seu banco de dados.'
+];
