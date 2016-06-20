a9aba9ee8fb9eb28a894258fd39692ce75adf7e9
boehsermoe <boehsermoe@me.com>
2016-06-20 11:24:19 +0200

    Redirect fixed

--- AdminModule.php
+++ AdminModule.php
@@ -25,7 +25,7 @@ class AdminModule extends \yii\base\Module implements BootstrapInterface
     {
         parent::init();
 
-        if(Yii::$app->cache === null){
+        if (Yii::$app->cache === null) {
             throw new \yii\web\ServerErrorHttpException('Please configure Cache component.');
         }
 
@@ -33,7 +33,8 @@ class AdminModule extends \yii\base\Module implements BootstrapInterface
             // Redirect to install process
             $this->on(Application::EVENT_BEFORE_ACTION, function (ActionEvent $event) {
                 if ($event->action->controller->id != 'install') {
-                    $event->action->controller->redirect(['/admin/install/index']);
+                    $response = $event->action->controller->redirect(['/admin/install/index']);
+                    Yii::$app->end(0, $response);
                 }
             });
         }
@@ -41,16 +42,18 @@ class AdminModule extends \yii\base\Module implements BootstrapInterface
         $this->activeModules = Module::findAllActive();
 
         $modules = [];
-        foreach($this->activeModules as $name => $module){
+        foreach ($this->activeModules as $name => $module) {
             $modules[$name]['class'] = $module->class;
-            if(is_array($module->settings)){
+            if (is_array($module->settings)) {
                 $modules[$name]['settings'] = $module->settings;
             }
         }
         $this->setModules($modules);
 
-        define('IS_ROOT',  !Yii::$app->user->isGuest && Yii::$app->user->identity->isRoot());
-        define('LIVE_EDIT', !Yii::$app->user->isGuest && Yii::$app->session->get('easyii_live_edit'));
+        if (Yii::$app instanceof \yii\web\Application) {
+            define('IS_ROOT', !Yii::$app->user->isGuest && Yii::$app->user->identity->isRoot());
+            define('LIVE_EDIT', !Yii::$app->user->isGuest && Yii::$app->session->get('easyii_live_edit'));
+        }
     }
 
 
@@ -58,7 +61,7 @@ class AdminModule extends \yii\base\Module implements BootstrapInterface
     {
         Yii::setAlias('easyii', '@vendor/noumo/easyii');
 
-        if(!$app->user->isGuest && strpos($app->request->pathInfo, 'admin') === false) {
+        if (!$app->user->isGuest && strpos($app->request->pathInfo, 'admin') === false) {
             $app->on(Application::EVENT_BEFORE_REQUEST, function () use ($app) {
                 $app->getView()->on(View::EVENT_BEGIN_BODY, [$this, 'renderToolbar']);
             });
@@ -73,7 +76,7 @@ class AdminModule extends \yii\base\Module implements BootstrapInterface
 
     public function getInstalled()
     {
-        if($this->_installed === null) {
+        if ($this->_installed === null) {
             try {
                 $this->_installed = Yii::$app->db->createCommand("SHOW TABLES LIKE 'easyii_%'")->query()->count() > 0 ? true : false;
             } catch (\Exception $e) {
