3f8b339a504b8ccfece3a2fae2ee02616ca3ddef
Alex Naumenko <5235960+shadik42@users.noreply.github.com>
2018-03-05 19:44:44 +0300

    add Ukrainian language for some modules

---
+++ modules/catalog/messages/uk/admin.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'Catalog' => 'Каталог',
+    'Category fields' => 'Поля категорії',
+    'Manage fields' => 'Редагувати поля',
+
+    'Add field' => 'Додати поле',
+    'Save fields' => 'Зберегти поля',
+    'Type options with `comma` as delimiter' => 'Елементи списку через кому',
+    'Fields' => 'Поля',
+
+    'Item created' => 'Новий запис успішно створено',
+    'Item updated' => 'Запис оновлено',
+    'Item deleted' => 'Запис видалено',
+
+    'Title' => 'Назва',
+    'Type' => 'Тип поля',
+    'Options' => 'Опції',
+    'Available' => 'Доступно',
+    'Price' => 'Ціна',
+    'Discount' => 'Знижка',
+    'Select' => 'Вибрати',
+];
